1a51c4b95ee3c98159f34ece97a2d6786fc5987e
Kedar Haldar <72819555+Anomalous-0@users.noreply.github.com>
2023-04-18 12:12:16 -0700

    Cleaner light and smoother doors?

--- Assets/Editor/InteractableEditor.cs
+++ Assets/Editor/InteractableEditor.cs
@@ -1,19 +1,29 @@
 using UnityEditor;
 
 [CustomEditor(typeof(Interactable), true)] 
-public class InteractableEditor : Editor {
+public class InteractableEditor : Editor
+{
     public override void OnInspectorGUI(){
         Interactable interactable = (Interactable)target;
-        base.OnInspectorGUI();
-        if(interactable.useEvents){
-                //we are using events. add the component
+        if(target.GetType() == typeof(EventOnlyInteractable)){
+            interactable.message = EditorGUILayout.TextField("Prompt Message", interactable.message);
+            EditorGUILayout.HelpBox("EventOnlyInteract can ONLY use UnityEvents.",MessageType.Info);
             if(interactable.GetComponent<InteractionEvent>() == null){
-                    interactable.gameObject.AddComponent<InteractionEvent>();
+                interactable.useEvents = true;
+                interactable.gameObject.AddComponent<InteractionEvent>();
             }
         } else{
+            base.OnInspectorGUI();
+            if(interactable.useEvents){
+                //we are using events. add the component
+                if(interactable.GetComponent<InteractionEvent>() == null){
+                    interactable.gameObject.AddComponent<InteractionEvent>();
+                }
+            } else{
                 // remove the component if not using the event
-            if(interactable.GetComponent<InteractionEvent>() != null){
-                DestroyImmediate(interactable.GetComponent<InteractionEvent>());
+                if(interactable.GetComponent<InteractionEvent>() != null){
+                    DestroyImmediate(interactable.GetComponent<InteractionEvent>());
+                }
             }
         }
     }
