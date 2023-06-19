df23a3e29c973cd2462bbc54fddee5313c34bd7a
Kedar Haldar <72819555+Anomalous-0@users.noreply.github.com>
2023-06-18 17:47:01 -0700

    I guess buttons don't work

--- Assets/Scripts/AnimationHandler.cs
+++ Assets/Scripts/AnimationHandler.cs
@@ -6,15 +6,15 @@ public class AnimationHandler : MonoBehaviour
 {
 
     public Animator animator;
-    
-    
+
+
     public void ToggleBool(string boolname){
         animator.SetBool(boolname,!animator.GetBool(boolname));
     }
 
-    public void PushButton(string boolname){
-        ToggleBool(boolname);
-        Invoke("ToggleBool", 1);
+    public void PushButton(string name){
+        animator.SetBool(name,!animator.GetBool(name));
+        Invoke("ToggleBool", 2.0f);
     }
 
 
