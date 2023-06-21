90935f8b23ccf2cd9c0e448b56378fcc0fbc7130
Kedar Haldar <72819555+Anomalous-0@users.noreply.github.com>
2023-06-20 17:29:29 -0700

    Revert "Revert "haven't done jumping yet""
    This reverts commit 1f41c8143cc442c7b69f69fb111a04dea73197d9.

--- Assets/Scripts/Player/Movement/AnimationStateController.cs
+++ Assets/Scripts/Player/Movement/AnimationStateController.cs
@@ -7,13 +7,22 @@ public class AnimationStateController : MonoBehaviour {
     Animator animator;
     int isWalkingHash;
     int isRunningHash;
-    private PlayerMotor playerMotor;
+    int isBackwardsHash;
+    int isJumpingHash;
+    // int isLeftHash;
+    // int isRightHash;
+    // private PlayerMotor playerMotor;
    
 
     void Start() {
         animator = GetComponent<Animator>();  
         isWalkingHash = Animator.StringToHash("isWalking");  
         isRunningHash = Animator.StringToHash("isRunning");
+        isBackwardsHash = Animator.StringToHash("isBackwards");
+        // isLeftHash = Animator.StringToHash("isLeft");
+        // isRightHash = Animator.StringToHash("isRight");
+        isJumpingHash = Animator.StringToHash("isJumping");
+
     }
 
     // Update is called once per frame
@@ -24,18 +33,65 @@ public class AnimationStateController : MonoBehaviour {
         bool run = Input.GetKey("left shift");
         bool isRunning = animator.GetBool(isRunningHash);
 
+        bool backward = Input.GetKey("s");
+        bool isBackwards = animator.GetBool(isBackwardsHash);
+
+        
+
+        // bool left = Input.GetKey("a");
+        // bool isLeft = animator.GetBool(isLeftHash);
+
+        // bool right = Input.GetKey("d");
+        // bool isRight = animator.GetBool(isRightHash);
+
+        
+
+        // if not walking and press w, walk
         if(!isWalking && forward){
             animator.SetBool(isWalkingHash, true);
         }
 
+        // if walking but not pressing w, stop
         if(isWalking && !forward){
             animator.SetBool(isWalkingHash, false);
         } 
 
+
+        // if not backwards and press s, walk
+        if(!isBackwards && backward){
+            animator.SetBool(isBackwardsHash, true);
+        }
+
+        // if backwards but not pressing s, stop
+        if(isBackwards && !backward){
+            animator.SetBool(isBackwardsHash, false);
+        } 
+
+        
+        // // Left and right: same principles
+        // if(!isLeft && left){
+        //     animator.SetBool(isLeftHash, true);
+        // }
+
+        // if(isLeft && !left){
+        //     animator.SetBool(isLeftHash, false);
+        // }
+
+        // if(!isRight && right){
+        //     animator.SetBool(isRightHash, true);
+        // }
+
+        // if(isRight && !right){
+        //     animator.SetBool(isRightHash, false);
+        // }
+
+
+        // if not running but shift and w is pressed, then run
         if(!isRunning && forward && run){
             animator.SetBool(isRunningHash, true);
         }
 
+        // if shift is released, or w, then stop
         if(isRunning && (!forward || !run)){
             animator.SetBool(isRunningHash, false);
         }
